5f202289a150c007c20ffd54404f7a1dcba99b10
Kyu Ha NA <danielna1118@snu.ac.kr>
2026-01-16 21:24:11 +0900

    navBar style change (#15)

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,43 @@
+import { FaComment, FaPlus, FaSearch, FaUser } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
+import './navBar.css';
+
+const NavBar = () => {
+  const location = useLocation();
+
+  const navLinks = [
+    { path: '/search-room', icon: <FaSearch />, label: 'roomSearch' },
+    { path: '/create-room', icon: <FaPlus />, label: 'roomCreate' },
+    { path: '/my-chat', icon: <FaComment />, label: 'myChat' },
+    { path: '/my-page', icon: <FaUser />, label: 'myPage' },
+  ];
+
+  return (
+    <nav className="navBar">
+      <div className="logo">
+        <Link to="/">
+          <img src="/snuxi-logo.png" alt="SNUXI Logo" />
+        </Link>
+      </div>
+      <div className="nav-links">
+        {navLinks.map(({ path, icon, label }) => (
+          <Link
+            key={path}
+            to={path}
+            className={`nav-item ${location.pathname === path ? 'active' : ''}`}
+          >
+            <div className="icon">{icon}</div>
+            <div className="label">{label}</div>
+          </Link>
+        ))}
+      </div>
+      <div className="login">
+        <Link to="/login" className="login-button">
+          로그인
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
